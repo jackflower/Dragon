63384e4f13f1943b8f665c443673a5e28bf622c6
jackflower <jackflower@poczta.onet.pl>
2020-11-10 16:51:18 +0100

    Update

--- Dragon/main.cpp
+++ Dragon/main.cpp
@@ -21,35 +21,56 @@ int main()
 
 	// s o l u t i o n
 
-	std::vector<Dragon *> smoczki;
+	//std::vector<Dragon *> smoczki;
+	//Dragon * ptr_dragon = nullptr;
+	//ptr_dragon = new Dragon(11,11);
+	//smoczki.push_back(ptr_dragon);
+	//ptr_dragon = new Dragon(22, 22);
+	//smoczki.push_back(ptr_dragon);
+	//ptr_dragon = new Dragon(33, 33);
+	//smoczki.push_back(ptr_dragon);
+	//Dragon * ptr_smoczyca = new Dragon(44, 44);
+	//ptr_smoczyca = ptr_dragon;
+	//smoczki.push_back(ptr_smoczyca);
+	//std::vector<Dragon *>::iterator it;
 
-	Dragon * ptr_dragon = nullptr;
+	//for (it = smoczki.begin(); it != smoczki.end(); it++)
+	//{
+	//	if (*it != nullptr)
+	//		delete *it;
+	//}
+	//smoczki.clear();
 
-	ptr_dragon = new Dragon(11,11);
-	smoczki.push_back(ptr_dragon);
-	
-	ptr_dragon = new Dragon(22, 22);
-	smoczki.push_back(ptr_dragon);
 
-	ptr_dragon = new Dragon(33, 33);
-	smoczki.push_back(ptr_dragon);
 
-	Dragon * ptr_smoczyca = new Dragon(44, 44);
-	ptr_smoczyca = ptr_dragon;
-	smoczki.push_back(ptr_smoczyca);
+	int jeden = 10;
+	int dwa = 20;
+	int trzy = 30;
 
+	int & one = jeden;
+	int & two = dwa;
+	int & three = trzy;
+
+	//std::vector<int> box;
+	//box.push_back(jeden);
+	//box.push_back(dwa);
+	//box.push_back(trzy);
+	//box.at(0) = 100;
+	//dwa = 200;
+
+	//std::vector<int*> referencje;
+	//referencje.push_back(&one);
+	//referencje.push_back(&two);
+	//referencje.push_back(&three);
+	////referencje.at(0) = &jeden;
+
+	int test = 123;
+	//referencje.at(0) = &test;
+
+	int w = one;
 
 	//
 	int warta = 0;
-
-	std::vector<Dragon *>::iterator it;
-
-	for (it = smoczki.begin(); it != smoczki.end(); it++)
-	{
-		if (*it != nullptr)
-			delete *it;
-	}
-	smoczki.clear();
 
 	return 0;
 }
